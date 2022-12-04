2719b7ee183a34dc672f4def5f3391d83f053797
bforbiggy <AndyHeCS@protonmail.com>
2022-12-04 14:07:22 -0500

    Not feeling comfy with line.Length/2 but it works

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,29 @@
 ﻿using System;
 
-public class Solution
+int charToValue(char c)
 {
-	public static void Main()
+	return Char.IsUpper(c) ? (c - 'A') + 27 : (c - 'a') + 1;
+}
+
+int total = 0;
+foreach (string line in File.ReadLines("input.txt"))
+{
+	// Store all char appearances of left side
+	HashSet<char> exists = new HashSet<char>();
+	for (int i = 0; i < line.Length / 2; i++)
 	{
-		Console.WriteLine("Initial commit.");
+		exists.Add(line[i]);
+	}
+
+	// Find shared char in right side
+	for (int i = line.Length / 2; i < line.Length; i++)
+	{
+		if (exists.Contains(line[i]))
+		{
+			total += charToValue(line[i]);
+			break;
+		}
 	}
 }
+
+Console.WriteLine(total);
