1397a23b4b0dee2a5150b37cb5deed29ebebb207
bforbiggy <AndyHeCS@protonmail.com>
2022-12-04 14:20:40 -0500

    I feel like I could have better variable names

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,29 @@
 ﻿using System;
 
-public class Solution
+// Returns true if range a is contained entirely within b
+bool rangeInRange(int[] a, int[] b)
 {
-	public static void Main()
+	return a[0] >= b[0] && a[1] <= b[1];
+}
+
+// Converts a range string (3-9) to a range array
+int[] stringToAssignment(string str)
+{
+	string[] tokens = str.Split("-");
+	return new int[] { Int32.Parse(tokens[0]), Int32.Parse(tokens[1]) };
+}
+
+int count = 0;
+foreach (string line in File.ReadLines("input.txt"))
+{
+	string[] assignments = line.Split(',');
+	int[] a = stringToAssignment(assignments[0]);
+	int[] b = stringToAssignment(assignments[1]);
+
+	if (rangeInRange(a, b) || rangeInRange(b, a))
 	{
-		Console.WriteLine("Initial commit.");
+		count++;
 	}
 }
+
+Console.WriteLine(count);
