f41d91168adcaeed7dbd650369b638ba7f2b84c9
bforbiggy <AndyHeCS@protonmail.com>
2022-12-06 00:59:24 -0500

    Tuning forks are so cool for being consistent

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,28 @@
 ﻿using System;
 
-public class Solution
+string input = File.ReadAllText("input.txt");
+
+int unique = 0;
+LinkedList<char> previous = new LinkedList<char>();
+for (int i = 0; i < input.Length; i++)
 {
-	public static void Main()
+	// Prune current list if necessary
+	if (previous.Count >= 4)
 	{
-		Console.WriteLine("Initial commit.");
+		char removed = previous.First.Value;
+		previous.RemoveFirst();
+		if (!previous.Any(match => match == removed))
+			unique--;
 	}
+
+	// Unique character being added: Update count
+	if (!previous.Any(match => match == input[i]))
+		unique++;
+	// Achieved unique character count: Solution found
+	if (unique == 4)
+	{
+		Console.WriteLine(i + 1);
+		break;
+	}
+	previous.AddLast(input[i]);
 }
