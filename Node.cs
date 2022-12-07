31de34d07b8996fdd07d9b234089ebf361d3db7f
bforbiggy <AndyHeCS@protonmail.com>
2022-12-07 00:59:31 -0500

    Massive refactor
    This fixes the double tree traversal

--- Node.cs
+++ Node.cs
@@ -1,9 +1,19 @@
+using System.Security.Cryptography;
+
 public class Node
 {
-	public int value = 0;
-	public string name = "";
-	public Node parent = null;
-	public List<Node> children = null; // TODO: Use dictionary for unique children
+	public int value;
+	public string name;
+	public Node parent;
+	public List<Node> children; // TODO: Use dictionary for unique children
+
+	public Node(int value = 0, string name = "", Node parent = null, List<Node> children = null)
+	{
+		this.value = value;
+		this.name = name;
+		this.parent = parent;
+		this.children = children;
+	}
 
 	public bool isDir()
 	{
@@ -13,5 +23,16 @@ public class Node
 	public bool isFile()
 	{
 		return children == null;
+	}
+
+	public void addChild(Node child)
+	{
+		children.Add(child);
+		Node temp = this;
+		while (temp != null)
+		{
+			temp.value += child.value;
+			temp = temp.parent;
+		}
 	}
 }
--- Solution.cs
+++ Solution.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 
 // Parsing setup
-Node root = new Node();
-root.children = new List<Node>();
+Node root = new Node(children: new List<Node>());
 string[] lines = File.ReadAllLines("input.txt");
 
 // Flesh out file system
@@ -20,7 +19,7 @@ for (int i = 0; i < lines.Length; i++)
 		else if (new_path.Equals("/"))
 			current = root;
 		else
-			current = current.children.Find((node) => node.name.Equals(new_path));
+			current = current.children.Find((node) => node.name.Equals(new_path))!;
 	}
 	// System command: ls
 	else if (line.StartsWith("$ ls"))
@@ -30,59 +29,33 @@ for (int i = 0; i < lines.Length; i++)
 	// Directory listing
 	else if (line.StartsWith("dir"))
 	{
-		Node directory = new Node();
-		directory.parent = current;
-		directory.name = line.Substring(4);
-		directory.children = new List<Node>();
-
-		current.children.Add(directory);
+		Node directory = new Node(name: line.Substring(4), parent: current, children: new List<Node>());
+		current.addChild(directory);
 	}
 	// File listing
 	else
 	{
 		string[] tokens = line.Split(' ');
-		Node file = new Node();
-		file.parent = current;
-		file.name = tokens[1];
-		file.value = Int32.Parse(tokens[0]);
-
-		current.children.Add(file);
+		Node file = new Node(name: tokens[1], value: Int32.Parse(tokens[0]), parent: current);
+		current.addChild(file);
 	}
 }
-
-// Traverse file system, tracking file size along the way
-int total_traverse(Node node)
-{
-	if (node.isFile())
-	{
-		return node.value;
-	}
-	else
-	{
-		foreach (Node child in node.children)
-			node.value += total_traverse(child);
-		return node.value;
-	}
-}
-int used = total_traverse(root);
 
 // Traverse file system, looking for smallest directory to delete
-int freeAmount = 70000000 - used;
+int freeAmount = 70000000 - root.value;
 int minimumFree = 30000000 - freeAmount;
 int smallest = Int32.MaxValue;
 void free_traverse(Node node)
 {
+	// Base case: Files are ignored
 	if (node.isFile())
-	{
 		return;
-	}
-	else
-	{
-		foreach (Node child in node.children)
-			free_traverse(child);
-		if (node.value >= minimumFree && node.value <= smallest)
-			smallest = node.value;
-	}
+
+	// Recursive case: Traverse children
+	foreach (Node child in node.children)
+		free_traverse(child);
+	if (node.value >= minimumFree && node.value <= smallest)
+		smallest = node.value;
 }
 free_traverse(root);
 Console.WriteLine(smallest);
