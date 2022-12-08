96aeb1c973a0e88ab73dfb8ae5512aa3f98aa958
bforbiggy <AndyHeCS@protonmail.com>
2022-12-08 00:16:47 -0500

    Probably don't need so many functions...

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,67 @@
 ﻿using System;
 
-public class Solution
+string[] lines = File.ReadAllLines("input.txt");
+
+// Construct 2D grid
+int[][] trees = new int[lines.Length][];
+for (int y = 0; y < trees.Length; y++)
 {
-	public static void Main()
+	trees[y] = new int[lines[y].Length];
+	for (int x = 0; x < trees[y].Length; x++)
+		trees[y][x] = lines[y][x] - '0';
+}
+
+bool visibleFromTop(int x, int y)
+{
+	int height = trees[y][x];
+	while (--y >= 0)
 	{
-		Console.WriteLine("Initial commit.");
+		if (trees[y][x] >= height)
+			return false;
+	}
+	return true;
+}
+
+bool visibleFromBottom(int x, int y)
+{
+	int height = trees[y][x];
+	while (++y < trees.Length)
+	{
+		if (trees[y][x] >= height)
+			return false;
+	}
+	return true;
+}
+
+bool visibleFromLeft(int x, int y)
+{
+	int height = trees[y][x];
+	while (--x >= 0)
+	{
+		if (trees[y][x] >= height)
+			return false;
+	}
+	return true;
+}
+
+bool visibleFromRight(int x, int y)
+{
+	int height = trees[y][x];
+	while (++x < trees[y].Length)
+	{
+		if (trees[y][x] >= height)
+			return false;
+	}
+	return true;
+}
+
+int total = 0;
+for (int y = 0; y < trees.Length; y++)
+{
+	for (int x = 0; x < trees[y].Length; x++)
+	{
+		if (visibleFromLeft(x, y) || visibleFromRight(x, y) || visibleFromTop(x, y) || visibleFromBottom(x, y))
+			total++;
 	}
 }
+Console.WriteLine(total);
