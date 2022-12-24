c0ac445edb63791e976a8434e090b98f5162dd54
bforbiggy <AndyHeCS@protonmail.com>
2022-12-24 01:45:53 -0500

    zzzz I give up

--- Solution.cs
+++ Solution.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
+LinkedList<Object> parseLine(string line)
+{
+	LinkedList<Object> result = new LinkedList<Object>();
+	return result;
+}
 
-Console.WriteLine("No real code, pick a branch for a specific problem.");
+string[] lines = File.ReadAllLines(@"input.txt");
+for (int i = 0; i < lines.Length; i += 3)
+{
+	Console.WriteLine(lines[i]);
+	Console.WriteLine(lines[i + 1]);
+}
